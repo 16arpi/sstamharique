9342c1572799a974fb28be26bafa71683d91d704
NiLuJe <ninuje@gmail.com>
2026-01-12 22:10:48 +0100

    Aaaand loading now makes sense, yay.
    Also, stuff works as expected, yay²

--- stt_amh/run_asr.py
+++ stt_amh/run_asr.py
@@ -24,9 +24,15 @@ app = typer.Typer()
 
 
 class InferenceContext:
-	def __init__(self) -> None:
+	def __init__(self, use_custom_adapter: bool = True) -> None:
 		self.target_lang = "amh"
-		self.model_id = "facebook/mms-1b-all"
+		self.custom_adapter = use_custom_adapter
+		if use_custom_adapter:
+			logger.info(f"Using our own <blue>{self.target_lang}</blue> adapter layers")
+			self.model_id = PRETRAINED_AMH
+		else:
+			logger.info(f"Using upstream <blue>{self.target_lang}</blue> adapter layers")
+			self.model_id = "facebook/mms-1b-all"
 		self.uroman = ur.Uroman()
 
 	def load_dataset(self) -> None:
@@ -35,22 +41,19 @@ class InferenceContext:
 		self.dataset = load_from_disk(CV_DATASET)
 		self.val_ds = self.dataset["validation"]
 
-	def load_model(self, use_custom_adapter: bool = True) -> None:
+	def load_model(self) -> None:
 		logger.info("Loading the model")
 
-		self.processor = AutoProcessor.from_pretrained(self.model_id, device_map="auto")
-		self.model = Wav2Vec2ForCTC.from_pretrained(self.model_id, device_map="auto")
+		self.processor = AutoProcessor.from_pretrained(
+			self.model_id, device_map="auto", local_files_only=self.custom_adapter
+		)
+		self.model = Wav2Vec2ForCTC.from_pretrained(
+			self.model_id, device_map="auto", local_files_only=self.custom_adapter
+		)
 
 		self.processor.tokenizer.set_target_lang(self.target_lang)
 
-		if use_custom_adapter:
-			logger.info(f"Using our own <blue>{self.target_lang}</blue> adapter layers")
-			self.model.load_adapter(
-				self.target_lang, local_files_only=True, cache_dir=PRETRAINED_AMH, use_safetensors=True
-			)
-		else:
-			logger.info(f"Using upstream <blue>{self.target_lang}</blue> adapter layers")
-			self.model.load_adapter(self.target_lang)
+		self.model.load_adapter(self.target_lang, local_files_only=self.custom_adapter)
 
 	def run_asr(self, sample: AudioDecoder, reference: str) -> tuple[str, str, float]:
 		inputs = self.processor(sample, sampling_rate=16_000, return_tensors="pt").to(self.model.device)
@@ -78,9 +81,9 @@ class InferenceContext:
 @app.command()
 def main(custom_adapter: Annotated[bool, typer.Option(help="Use our own custom adapter")] = True) -> None:
 	"""Run a full inference test on the test dataset"""
-	ctx = InferenceContext()
+	ctx = InferenceContext(custom_adapter)
 	ctx.load_dataset()
-	ctx.load_model(custom_adapter)
+	ctx.load_model()
 
 	results = []
 	for i, data_point in enumerate(tqdm(ctx.dataset["test"], desc="Inferencing...")):
